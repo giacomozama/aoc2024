bbf90421f44c4a9f8cea96e29744eccefe8807b3
Giacomo Zama <giacomozama@hotmail.it>
2024-12-04 21:22:09 +0100

    day4

--- src/main/kotlin/st/contraptioni/aoc2024/problems/Problem4.kt
+++ src/main/kotlin/st/contraptioni/aoc2024/problems/Problem4.kt
@@ -1,18 +1,54 @@
 package st.contraptioni.aoc2024.problems
 
-import st.contraptioni.aoc2024.NotImplemented
 import st.contraptioni.aoc2024.Problem
 
-class Problem4 : Problem<Unit, NotImplemented, NotImplemented>() {
-    
-    override fun parseInput(rawInput: String) {
-    }
-    
-    override fun solvePart1(input: Unit): NotImplemented {
-        return NotImplemented
+class Problem4 : Problem<Array<CharArray>, Int, Int>() {
+
+    override fun parseInput(rawInput: String): Array<CharArray> {
+        return rawInput.lines().map { it.toCharArray() }.toTypedArray()
     }
 
-    override fun solvePart2(input: Unit): NotImplemented {
-        return NotImplemented
+    override fun solvePart1(input: Array<CharArray>): Int {
+        fun check(y: Int, x: Int, dy: Int, dx: Int): Boolean {
+            return y + dy * 3 in input.indices &&
+                x + dx * 3 in input[0].indices &&
+                (0..3).all { input[y + it * dy][x + it * dx] == "XMAS"[it] }
+        }
+
+        var ans = 0
+
+        for (y in input.indices) {
+            for (x in input[0].indices) {
+                ans += if (check(y, x, -1, 0)) 1 else 0
+                ans += if (check(y, x, -1, 1)) 1 else 0
+                ans += if (check(y, x, 0, 1)) 1 else 0
+                ans += if (check(y, x, 1, 1)) 1 else 0
+                ans += if (check(y, x, 1, 0)) 1 else 0
+                ans += if (check(y, x, -1, -1)) 1 else 0
+                ans += if (check(y, x, 0, -1)) 1 else 0
+                ans += if (check(y, x, 1, -1)) 1 else 0
+            }
+        }
+
+        return ans
+    }
+
+    override fun solvePart2(input: Array<CharArray>): Int {
+        var ans = 0
+
+        for (y in 1 until input.lastIndex) {
+            for (x in 1 until input[0].lastIndex) {
+                if (input[y][x] != 'A') continue
+                val tl = input[y - 1][x - 1]
+                val tr = input[y - 1][x + 1]
+                val bl = input[y + 1][x - 1]
+                val br = input[y + 1][x + 1]
+                val d1 = tl == 'M' && br == 'S' || tl == 'S' && br == 'M'
+                val d2 = tr == 'M' && bl == 'S' || tr == 'S' && bl == 'M'
+                if (d1 && d2) ans++
+            }
+        }
+
+        return ans
     }
 }
