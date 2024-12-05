a456159ea9dbbed7677a33142757d1dd79093bba
Giacomo Zama <giacomozama@hotmail.it>
2024-12-05 18:10:10 +0100

    day5

--- src/main/kotlin/st/contraptioni/aoc2024/problems/Problem5.kt
+++ src/main/kotlin/st/contraptioni/aoc2024/problems/Problem5.kt
@@ -1,18 +1,73 @@
 package st.contraptioni.aoc2024.problems
 
-import st.contraptioni.aoc2024.NotImplemented
 import st.contraptioni.aoc2024.Problem
 
-class Problem5 : Problem<Unit, NotImplemented, NotImplemented>() {
-    
-    override fun parseInput(rawInput: String) {
-    }
-    
-    override fun solvePart1(input: Unit): NotImplemented {
-        return NotImplemented
+class Problem5 : Problem<Problem5.Input, Int, Int>() {
+
+    class Input(
+        val ordering: Set<List<Int>>,
+        val updates: List<List<Int>>
+    )
+
+    override fun parseInput(rawInput: String): Input {
+        val (rawOrdering, rawUpdates) = rawInput.split("\n\n")
+        val ordering = rawOrdering.lines().map { l -> l.split("|").map { it.toInt() } }.toSet()
+        val updates = rawUpdates.lines().map { l -> l.split(",").map { it.toInt() } }
+        return Input(ordering, updates)
     }
 
-    override fun solvePart2(input: Unit): NotImplemented {
-        return NotImplemented
+    override fun solvePart1(input: Input): Int {
+        var ans = 0
+
+        updates@ for (update in input.updates) {
+            for (i in 1 until update.size) {
+                for (j in 0 until i) {
+                    if (listOf(update[i], update[j]) in input.ordering) continue@updates
+                }
+            }
+
+            ans += update[update.size / 2]
+        }
+
+        return ans
+    }
+
+    override fun solvePart2(input: Input): Int {
+        var ans = 0
+
+        for (update in input.updates) {
+            var isSorted = true
+
+            check@ for (i in 1 until update.size) {
+                for (j in 0 until i) {
+                    if (listOf(update[i], update[j]) in input.ordering) {
+                        isSorted = false
+                        break@check
+                    }
+                }
+            }
+
+            if (isSorted) continue
+
+            val sorted = update.toIntArray()
+
+            sorting@ while (!isSorted) {
+                for (i in 1 until update.size) {
+                    for (j in 0 until i) {
+                        if (listOf(sorted[i], sorted[j]) in input.ordering) {
+                            val t = sorted[i]
+                            sorted[i] = sorted[j]
+                            sorted[j] = t
+                            continue@sorting
+                        }
+                    }
+                }
+                isSorted = true
+            }
+
+            ans += sorted[sorted.size / 2]
+        }
+
+        return ans
     }
 }
