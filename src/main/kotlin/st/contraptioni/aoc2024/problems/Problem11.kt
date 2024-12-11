6a8b8d2131398373a80df2b81135fbe6655629e4
Giacomo Zama <giacomozama@hotmail.it>
2024-12-11 19:04:23 +0100

    day11

--- src/main/kotlin/st/contraptioni/aoc2024/problems/Problem11.kt
+++ src/main/kotlin/st/contraptioni/aoc2024/problems/Problem11.kt
@@ -1,18 +1,49 @@
 package st.contraptioni.aoc2024.problems
 
-import st.contraptioni.aoc2024.NotImplemented
 import st.contraptioni.aoc2024.Problem
 
-class Problem11 : Problem<Unit, NotImplemented, NotImplemented>() {
-    
-    override fun parseInput(rawInput: String) {
+class Problem11 : Problem<IntArray, Long, Long>() {
+
+    override fun parseInput(rawInput: String): IntArray {
+        return rawInput.split(" ").map { it.toInt() }.toIntArray()
     }
 
-    override fun solvePart1(input: Unit): NotImplemented {
-        return NotImplemented
+    private fun solveForIterations(input: IntArray, iterations: Int): Long {
+        val memo = hashMapOf<Long, Long>()
+
+        fun calc(n: Long, remaining: Int): Long {
+            if (remaining == 0) return 1
+
+            return memo.getOrPut(n * 100L + remaining) {
+                if (n == 0L) {
+                    calc(1, remaining - 1)
+                } else {
+                    var r = n
+                    var digitCount = 0
+                    while (r > 0) {
+                        r /= 10
+                        digitCount++
+                    }
+
+                    if (digitCount and 1 == 0) {
+                        var mask = 1
+                        for (k in 1..digitCount / 2) mask *= 10
+                        calc(n / mask, remaining - 1) + calc(n % mask, remaining - 1)
+                    } else {
+                        calc(n * 2024, remaining - 1)
+                    }
+                }
+            }
+        }
+
+        return input.sumOf { calc(it.toLong(), iterations) }
     }
 
-    override fun solvePart2(input: Unit): NotImplemented {
-        return NotImplemented
+    override fun solvePart1(input: IntArray): Long {
+        return solveForIterations(input, 25)
+    }
+
+    override fun solvePart2(input: IntArray): Long {
+        return solveForIterations(input, 75)
     }
 }
