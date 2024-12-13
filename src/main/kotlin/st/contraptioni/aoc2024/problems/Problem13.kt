c4021d82fd92d1e2012b2a18117db3fd40f76f1d
Giacomo Zama <giacomozama@hotmail.it>
2024-12-13 18:28:04 +0100

    day13

--- src/main/kotlin/st/contraptioni/aoc2024/problems/Problem13.kt
+++ src/main/kotlin/st/contraptioni/aoc2024/problems/Problem13.kt
@@ -1,18 +1,47 @@
 package st.contraptioni.aoc2024.problems
 
-import st.contraptioni.aoc2024.NotImplemented
 import st.contraptioni.aoc2024.Problem
 
-class Problem13 : Problem<Unit, NotImplemented, NotImplemented>() {
-    
-    override fun parseInput(rawInput: String) {
+class Problem13 : Problem<List<IntArray>, Long, Long>() {
+
+    override fun parseInput(rawInput: String): List<IntArray> {
+        val regex = """
+            Button A: X\+(\d+), Y\+(\d+)
+            Button B: X\+(\d+), Y\+(\d+)
+            Prize: X=(\d+), Y=(\d+)
+        """.trimIndent().toRegex()
+
+        return regex.findAll(rawInput).map { res -> IntArray(6) { res.groupValues[it + 1].toInt() } }.toList()
     }
 
-    override fun solvePart1(input: Unit): NotImplemented {
-        return NotImplemented
+    private fun solveFor(machine: IntArray, prizeOffset: Long): Long {
+        val (aX, aY, bX, bY) = machine
+        val prizeX = machine[4] + prizeOffset
+        val prizeY = machine[5] + prizeOffset
+
+        // we need to solve the linear system
+        //
+        // aY * A + bY * B = prizeY
+        // aX * A + bX * B = prizeX
+        //
+        // for A and B, only accepting integer solutions
+
+        val d = aY * bX - aX * bY
+
+        val dA = prizeY * bX - prizeX * bY
+        if (dA % d != 0L) return 0L
+
+        val dB = aY * prizeX - aX * prizeY
+        if (dB % d != 0L) return 0L
+
+        return dA / d * 3 + dB / d
     }
 
-    override fun solvePart2(input: Unit): NotImplemented {
-        return NotImplemented
+    override fun solvePart1(input: List<IntArray>): Long {
+        return input.sumOf { solveFor(it, 0) }
+    }
+
+    override fun solvePart2(input: List<IntArray>): Long {
+        return input.sumOf { solveFor(it, 10_000_000_000_000) }
     }
 }
